42e86c80f3cfee5c5d6487c183d31b7a14be3bfd
sheharyark017 <sheharyark017@gmail.com>
2022-03-24 11:45:19 +0500

    hover dropdowns

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+const buttonContainer = document.querySelector(".slider-bottom-btn");
+const buttonContents = document.querySelectorAll(".hover-dropdown");
+const blur = document.querySelector(".blur");
+
+buttonContainer.addEventListener("mouseover", function (e) {
+  const clicked = e.target.closest(".slider-button");
+
+  if (!clicked) {
+    return;
+  }
+
+  buttonContents.forEach((buttonContent) =>
+    buttonContent.classList.remove("hover-dropdown-active")
+  );
+
+  document
+    .querySelector(`.hover-dropdown-${clicked.dataset.button}`)
+    .classList.add("hover-dropdown-active");
+
+  blur.classList.add("blur-active");
+});
+
+buttonContainer.addEventListener("mouseout", function (e) {
+  buttonContents.forEach((buttonContent) =>
+    buttonContent.classList.remove("hover-dropdown-active")
+  );
+  blur.classList.remove("blur-active");
+});
+
 var lastScroll = 0;
 
 jQuery(document).ready(function ($) {
@@ -51,6 +80,34 @@ var swiper = new Swiper(".mySwiper", {
     1024: {
       slidesPerView: 2,
       spaceBetween: 50,
+    },
+  },
+});
+
+var swiper = new Swiper(".mySwiper-hover", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 15,
     },
   },
 });
